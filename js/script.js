df1e612d0c9d6cbb55e9606d39cd934bde23c482
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2022-12-07 19:48:42 +0000

    init

--- js/script.js
+++ js/script.js
@@ -10,14 +10,23 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-07-JS/sw.js", {
+    scope: "/ICS2O-Unit5-07-JS/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function ccalculates the sum of natural numbers.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  var numN = parseFloat(document.getElementById("integer-n").value)
+  var sum = 0
+  var counter = 0
+  var loop = 0
+
+  for (let loop = 0; loop < numN; loop++) {
+    counter++
+    sum = sum + counter
+    document.getElementById("sum").innerHTML = "The sum is " + sum + ".<br>"
+  }
 }
